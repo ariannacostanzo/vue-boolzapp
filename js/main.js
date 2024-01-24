30f13152cdb3a211e2b374accb35990ac45ac787
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-24 11:24:24 +0100

    inizializing app

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 // console.log(Vue)
 
+const {createApp} = Vue;
+
+const app = createApp ({
+    name: 'Boolzapp',
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+
+
+
+
+});
+
+
+app.mount('#root');
