0986ad92053ad4948df549d813451babbffb1aa2
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-24 14:57:55 +0100

    implementing logic for changin chat

--- js/main.js
+++ js/main.js
@@ -7,17 +7,31 @@ const app = createApp ({
     data() {
         return {
             data,
+            userID: 0,
         }
     },
     computed: {
         contacts() {
             return this.data.contacts
-        } 
+        },
+        // messages() {
+        //     return this.contacts.messages
+        // }
             
     },
     methods: {
-        log(id) {
+        filteredMessages(id) {
             console.log(id)
+            const filteredMessages = this.contacts.filter((contact) => {
+                if (id === contact.id) {
+                    console.log('è uguale')
+                } else {
+                    console.log(' non è uguale')
+                }
+            })
+
+            console.log(filteredMessages)
+            return filteredMessages
         }
     }
 
